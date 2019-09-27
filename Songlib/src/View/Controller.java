b860bf1edac748906845e8a536f1c175f063d24c
Allen He <47501960+AllenHeeee@users.noreply.github.com>
2019-09-26 21:16:27 -0400

    Add files via upload

---
+++ Songlib/src/View/Controller.java
@@ -0,0 +1,29 @@
+package View;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller {
+    @FXML Label SongNameShow;
+
+    public void LastSongTapped(ActionEvent e) {
+
+    }
+    public void NextSongTapped(ActionEvent e) {
+
+    }
+    public void ExitTapped(ActionEvent e) {
+
+    }
+    public void EditTapped(ActionEvent e) {
+
+    }
+    public void DeleteTapped(ActionEvent e) {
+
+    }
+    public void AddTapped(ActionEvent e) {
+        SongNameShow.setText("Good");
+    }
+
+}
