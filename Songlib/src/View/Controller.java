7552295cf19d149390242ae54118ed159af092cf
Allen He <47501960+AllenHeeee@users.noreply.github.com>
2019-09-28 14:35:39 -0400

    Add files via upload

--- Songlib/src/View/Controller.java
+++ Songlib/src/View/Controller.java
@@ -58,14 +58,7 @@ public class Controller implements Initializable {    //   修改过
     }
     public void ChooseTapped(ActionEvent e) {
         SongList.currIndex = SongListUI.getSelectionModel().getSelectedIndex();
-        SongNameEdit.setText(SongList.list.get(SongList.currIndex).getSongName());
-        SongNameShow.setText(SongList.list.get(SongList.currIndex).getSongName());
-        ArtistEdit.setText(SongList.list.get(SongList.currIndex).getArtist());
-        AuthorShow.setText("By: "+SongList.list.get(SongList.currIndex).getArtist());
-        AlbumEdit.setText(SongList.list.get(SongList.currIndex).getAlbum());
-        AlbumShow.setText("Album: "+SongList.list.get(SongList.currIndex).getAlbum());
-        YearEdit.setText(SongList.list.get(SongList.currIndex).getYear());
-        YearShow.setText("Year: "+SongList.list.get(SongList.currIndex).getYear());
+        retrieveData(SongList.currIndex);
     }
     public void AddTapped(ActionEvent e) {
         String songname=SongNameAdd.getText();
@@ -137,16 +130,19 @@ public class Controller implements Initializable {    //   修改过
             if(SongList.list.get(0).getSongName()!=null) {
                 SongList.currIndex=0;
                 /// Edited!
-                SongNameEdit.setText(SongList.list.get(SongList.currIndex).getSongName());
-                SongNameShow.setText(SongList.list.get(SongList.currIndex).getSongName());
-                ArtistEdit.setText(SongList.list.get(SongList.currIndex).getArtist());
-                AuthorShow.setText("By: "+SongList.list.get(SongList.currIndex).getArtist());
-                AlbumEdit.setText(SongList.list.get(SongList.currIndex).getAlbum());
-                AlbumShow.setText("Album: "+SongList.list.get(SongList.currIndex).getAlbum());
-                YearEdit.setText(SongList.list.get(SongList.currIndex).getYear());
-                YearShow.setText("Year: "+SongList.list.get(SongList.currIndex).getYear());
+                retrieveData(SongList.currIndex);
             }
         }
+    }
+    public void retrieveData(int index){
+        SongNameEdit.setText(SongList.list.get(index).getSongName());
+        SongNameShow.setText(SongList.list.get(index).getSongName());
+        ArtistEdit.setText(SongList.list.get(index).getArtist());
+        AuthorShow.setText("By: "+SongList.list.get(index).getArtist());
+        AlbumEdit.setText(SongList.list.get(index).getAlbum());
+        AlbumShow.setText("Album: "+SongList.list.get(index).getAlbum());
+        YearEdit.setText(SongList.list.get(index).getYear());
+        YearShow.setText("Year: "+SongList.list.get(index).getYear());
     }
     private void loadData(){
         obslist.removeAll(obslist);
